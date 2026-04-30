72e4285d69f95276da17455a137c3598f1a681dd
testkun <154046906+testkun08080@users.noreply.github.com>
2026-04-30 19:29:45 +0700

    update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,11 +4,19 @@ import vike from "vike/plugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
-  const repoBase = "/testkun08080.github.io/";
-
+  // const repoBase = "/testkun08080.github.io/";
+  const repoBase = "/";
   return {
     base: repoBase,
     plugins: [tailwindcss(), vike()],
+    resolve: {
+      alias:
+        mode === "production"
+          ? {
+              leva: new URL("./debug/leva-stub.tsx", import.meta.url).pathname,
+            }
+          : undefined,
+    },
     server: {
       // Keep dev server private (localhost only)
       // host: "127.0.0.1",
