d437994a820c0dd04b8309cc758d7a67405f56a9
testkun <154046906+testkun08080@users.noreply.github.com>
2026-05-07 13:28:53 +0700

    omit

--- app/pages/+Layout.tsx
+++ app/pages/+Layout.tsx
@@ -1,14 +1,17 @@
 import type { ReactNode } from "react";
 import { ScrollShell } from "../components/ScrollShell";
+import { LanguageProvider } from "../lib/LanguageContext";
 import "./Layout.css";
 import "./tailwind.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <ScrollShell>
-      <div id="page-content" className="relative z-10 min-h-dvh">
-        {children}
-      </div>
-    </ScrollShell>
+    <LanguageProvider>
+      <ScrollShell>
+        <div id="page-content" className="relative z-10 min-h-dvh">
+          {children}
+        </div>
+      </ScrollShell>
+    </LanguageProvider>
   );
 }
